5cf407b939b3b2638ae54e95c5bfb5c975044895
GeekSunil <sunil.innovative@gmail.com>
2016-08-16 18:03:47 +0800

    sample

---
+++ AngularProject/CustomScripts/TextFunction.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="c:\users\sunil\documents\visual studio 2015\Projects\AngularProject\AngularProject\scripts/angular.js" />
+var app = angular.module("myApp", []);
+
+
+
+app.controller("MyContl", function ($scope,$interval) {
+  $scope.intl   = new Date().toLocaleTimeString();
+  $interval(function () {
+      $scope.theTime = new Date().toLocaleTimeString();
+  }, 1000);
+   
+});
